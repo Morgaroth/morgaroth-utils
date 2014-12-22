775bed5b509cc3a64f90ec8c5849c2457116d0a3
Mateusz Jaje <mateuszjaje@gmail.com>
2014-12-23 00:07:10 +0100

    added cakes to package object

--- spray/src/main/scala/io/github/morgaroth/utils/spray/auth/package.scala
+++ spray/src/main/scala/io/github/morgaroth/utils/spray/auth/package.scala
@@ -5,10 +5,36 @@ import io.github.morgaroth.utils.spray.auth.mongoimpl._
 package object auth {
 
   object mongo {
-    trait MongoUserWithSelfUser extends UsersDBConfig with DefaultMongoUserDAO
-    trait MongoSession[UserType <: IdAble] extends SessionsDBConfig with DefaultMongoSessionDAO[UserType]
-    trait MongoSessionWithSelfUser extends MongoSession[DefaultMongoUser]
+    object user {
+      trait MongoUserWithSelfUser
+        extends UsersDBConfig
+        with DefaultMongoUserDAO
+    }
+
+    object session {
+      trait MongoSession[UserType <: IdAble]
+        extends SessionsDBConfig
+        with DefaultMongoSessionDAO[UserType]
+
+      trait MongoSessionWithSelfUser
+        extends MongoSession[DefaultMongoUser]
+    }
   }
 
+  trait AuthService[UserType <: IdAble, SessionType]
+    extends UserDAO[UserType]
+    with SessionDAO[UserType, SessionType]
+    with DefaultTokenGenerator
+    with AuthServiceAuthentication[UserType, SessionType]
+    with AuthServiceAPI[UserType, SessionType]
+
+  trait MongoAuthService[UserType <: IdAble]
+    extends mongo.session.MongoSessionWithSelfUser
+    with AuthService[UserType, DefaultMongoSession]
+
+  trait MongoAuthSelfService
+    extends mongo.user.MongoUserWithSelfUser
+    with mongo.session.MongoSessionWithSelfUser
+    with MongoAuthService[DefaultMongoUser]
 
 }
